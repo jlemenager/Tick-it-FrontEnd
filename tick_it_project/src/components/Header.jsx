ed920be666bc276d958924eb7a72b3dc5a42c320
jlemenager <jeremy@unitedbyzero.com>
2023-07-21 13:30:48 -0400

    style edits

--- tick_it_project/src/components/Header.jsx
+++ tick_it_project/src/components/Header.jsx
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom'
 export default function Header(){
 
     return(
         <div className='headercont'>
-            <img className='logo' src="src/images/logo.png" alt="logo" />
+            <Link to='/'><img className='logo' src="src/images/logo.png" alt="logo" /></Link>
             <h3 id='title'>Front Row</h3>
         </div>
     )
--- tick_it_project/src/components/Home.jsx
+++ tick_it_project/src/components/Home.jsx
@@ -1,37 +1,39 @@
 
 export default function Home(){
-    let placeForTypewriterText = ''
-    let typewriterText = 'concerts'
-    let l = 0
-    const typeSpeed = 150
-    const typingEffect = () => {
-        if (l<typewriterText.length && typewriterText === 'concerts'){
-            placeForTypewriterText += typewriterText.charAt(l)
-            l++
-            setTimeout(typingEffect,typeSpeed)
-        } else if (l===typewriterText.length && typewriterText === 'concerts'){
-            setTimeout(()=>{
-            l=0
-            typewriterText = 'sports'
-            placeForTypewriterText = ''
-            setTimeout(typingEffect, 100)
-            }
-            ,700)
-        } else if (l<typewriterText.length && typewriterText === 'sports'){
-            placeForTypewriterText += typewriterText.charAt(l)
-            l++
-            setTimeout(typingEffect,typeSpeed)
-        } else if (l===typewriterText.length && typewriterText === 'sports'){
-            setTimeout(()=>{
-            l=0
-            typewriterText = 'concerts'
-            placeForTypewriterText = ''
-            setTimeout(typingEffect,100)
-        },700)
-        }
-    }
-    typingEffect()
+    // let placeForTypewriterText = ''
+    // let typewriterText = 'concerts'
+    // let l = 0
+    // const typeSpeed = 150
+    // const typingEffect = () => {
+    //     if (l<typewriterText.length && typewriterText === 'concerts'){
+    //         placeForTypewriterText += typewriterText.charAt(l)
+    //         l++
+    //         setTimeout(typingEffect,typeSpeed)
+    //     } else if (l===typewriterText.length && typewriterText === 'concerts'){
+    //         setTimeout(()=>{
+    //         l=0
+    //         typewriterText = 'sports'
+    //         placeForTypewriterText = ''
+    //         setTimeout(typingEffect, 100)
+    //         }
+    //         ,700)
+    //     } else if (l<typewriterText.length && typewriterText === 'sports'){
+    //         placeForTypewriterText += typewriterText.charAt(l)
+    //         l++
+    //         setTimeout(typingEffect,typeSpeed)
+    //     } else if (l===typewriterText.length && typewriterText === 'sports'){
+    //         setTimeout(()=>{
+    //         l=0
+    //         typewriterText = 'concerts'
+    //         placeForTypewriterText = ''
+    //         setTimeout(typingEffect,100)
+    //     },700)
+    //     }
+    // }
+    // typingEffect()
     return(
-        <h1>Want to go to a <span>concert or sporting event</span>?</h1>
+        <div>
+            <h1 className='frontPageH1'>What are you doing this weekend?</h1>
+        </div>
     )
 }
